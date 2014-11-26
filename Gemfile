59ca1aa27dddebdc8d9f23dd21c224562aa940f6
homeofmatt <conge101@mail.chapman.edu>
2014-11-26 13:52:48 -0800

    script successfully connects to podio, grabs an item, & can parse it. Hook test implemented

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ gem 'nokogiri'
 gem 'rack', '~>1.1'
 gem 'rspec', :require => 'spec'
 gem 'sinatra'
+gem 'podio'
+gem "octokit", "~> 3.0"
---
+++ src/issue_tracker.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'podio'
+require 'octokit'
+
+Podio.setup(:api_key => 'issues', :api_secret => 'QnVQnkCQkCBBsYiVWcdVpuQS3TlvYaDfc3xacXj9n2bNvULAYCOg4MM9TOV5LGaq')
+Podio.client.authenticate_with_app('9343326', '8a6c2571599e470d8dbaae867a70ce94')
+
+
+client = Octokit::Client.new \
+	:login    => 'CharlesChapman',
+	:password => 'M@rket2009'
+
+issue = Podio::Item.find_basic(195879868)
+
+
+puts issue[:app]
+
+
+case params['type']
+	when 'hook.verify'
+		# Validate the webhook
+		Podio::Hook.validate(params['hook_id'], params['code'])
+	when 'item.create'
+		puts "Item created!"
+	when 'item.update'
+		puts "Item updated!"
+	when 'item.delete'
+		puts "Item deleted!"
+	else
+		puts "Invalid hook verify."
+end
+
+get '/' do
+  if issue
+  	'hello world'
+  else
+  	'hello noooo :('
+  end
+end
