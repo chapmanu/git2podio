89f0c2b5bf9050097d2de3d594c7b2d32fe74bb9
homeofmatt <conge101@mail.chapman.edu>
2014-11-26 14:32:14 -0800

    edit

--- src/issue_tracker.rb
+++ src/issue_tracker.rb
@@ -2,21 +2,26 @@ require 'sinatra'
 require 'podio'
 require 'octokit'
 
+#podio login & client object setup
 Podio.setup(:api_key => 'issues', :api_secret => 'QnVQnkCQkCBBsYiVWcdVpuQS3TlvYaDfc3xacXj9n2bNvULAYCOg4MM9TOV5LGaq')
 Podio.client.authenticate_with_app('9343326', '8a6c2571599e470d8dbaae867a70ce94')
 
-
+#github login
 client = Octokit::Client.new \
 	:login    => 'CharlesChapman',
 	:password => 'M@rket2009'
 
 issue = Podio::Item.find_basic(195879868)
-
-
 puts issue[:app]
 
+get '/' do
+  if issue
+  	'hello world'
+  else
+  	'hello noooo :('
+  end
 
-case params['type']
+  case params['type']
 	when 'hook.verify'
 		# Validate the webhook
 		Podio::Hook.validate(params['hook_id'], params['code'])
@@ -28,12 +33,5 @@ case params['type']
 		puts "Item deleted!"
 	else
 		puts "Invalid hook verify."
-end
-
-get '/' do
-  if issue
-  	'hello world'
-  else
-  	'hello noooo :('
-  end
+	end
 end
