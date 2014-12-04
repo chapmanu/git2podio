e85d18fcd96b2c402dc051b89a16433c937a476e
homeofmatt <conge101@mail.chapman.edu>
2014-12-04 11:18:06 -0800

    debugq

--- src/issue_tracker.rb
+++ src/issue_tracker.rb
@@ -12,8 +12,9 @@ post '/' do
 
   #github login
   client = Octokit::Client.new :login => 'CharlesChapman', :password => 'M@rket2009'
-
-  puts "Parameters: #{params.inspect}"
+  
+  puts "Login name:"
+  puts client.user.login
 
   case params['type']
 	when 'hook.verify'
@@ -25,11 +26,11 @@ post '/' do
 		puts "Item created!"
 		issue = Podio::Item.find_basic(params['item_id'])
 
-		#puts "Description:"
-		puts issue.attributes[:fields][1]["values"][0]["value"][3..-5]
+		title = issue.attributes[:title]
+		desc = issue.attributes[:fields][1]["values"][0]["value"][3..-5]
 
 		#figure out sending podio info to github through project name.
-		#client.create_issue("chapmanu/git2podio", issue.attributes[:title], issue.attributes[:fields][1]["values"][0]["value"][3..-5])
+		#client.create_issue("chapmanu/git2podio", title, desc)
 	when 'item.update'
 		puts "Item updated!"
 
