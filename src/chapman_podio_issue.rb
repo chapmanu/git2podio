c7ad3815b4cec1a15eba2595cfb40554df8773f6
homeofmatt <conge101@mail.chapman.edu>
2014-12-11 09:56:39 -0800

    debug

--- src/chapman_podio_issue.rb
+++ src/chapman_podio_issue.rb
@@ -15,51 +15,6 @@ class ChapmanPodioIssue
 
 	FIELDS_MAP = {title: '72675682', issue_number: '72678301', description: '72676401', project: '72676406', category: '80284837', status: '72676409', assigned_to: '72676405', reported_by: '72676404'}
 
-	def create_on_github
-		title       = get_title
-		issue_num   = get_issue_number
-		desc        = get_description
-		repo        = get_repo
-		category    = get_category
-		status      = get_status
-		assigned_to = get_assigned_to
-		reported_by = get_reported_by
-
-		#determine which repo to send issue to
-		case repo
-			when 'Social', 'Inside', 'Events'
-				repo = "chapmanu/inside"
-			when 'Blogs'
-				repo = "chapmanu/cu-wp-template"
-			when 'Homepage'
-				repo = "chapmanu/web-components"
-			else
-				puts "Invalid Podio issue made: #{title}."
-				repo = "chapmanu/git2podio"
-		end
- 		
- 		#create issue in github and give it correct tag
-		git_issue = nil
-		case category
-			when 'Bug'
-				git_issue = client.create_issue(repo, title, desc, {:labels => ["bug"]})
-			when 'Enhancement'
-				git_issue = client.create_issue(repo, title, desc, {:labels => ["enhancement"]})
-			when 'Question'
-				git_issue = client.create_issue(repo, title, desc, {:labels => ["question"]})
-			else
-				git_issue = client.create_issue(repo, title, desc)
-		end
-
-		#update the Podio item id with the corresponding github issue id
-		Podio::ItemField.update(params['item_id'], FIELDS_MAP[:issue_number], {:value => git_issue[:number].to_s}, {:hook => false})
-
-		#close the issue on github if the status on Podio is set to complete
-		if status == "Complete"
-			client.close_issue(repo, git_issue[:number])
-		end
-	end
-
 	##### Get all individual fields #####
 	def get_title
 		title = @fields_hash[FIELDS_MAP[:title]]
@@ -135,3 +90,49 @@ class ChapmanPodioIssue
 
 	private :get_title, :get_issue_number, :get_description, :get_repo, :get_category, :get_status, :get_assigned_to, :get_reported_by
 end
+
+#create issue on github
+def create_on_github
+		title       = get_title
+		issue_num   = get_issue_number
+		desc        = get_description
+		repo        = get_repo
+		category    = get_category
+		status      = get_status
+		assigned_to = get_assigned_to
+		reported_by = get_reported_by
+
+		#determine which repo to send issue to
+		case repo
+			when 'Social', 'Inside', 'Events'
+				repo = "chapmanu/inside"
+			when 'Blogs'
+				repo = "chapmanu/cu-wp-template"
+			when 'Homepage'
+				repo = "chapmanu/web-components"
+			else
+				puts "Invalid Podio issue made: #{title}."
+				repo = "chapmanu/git2podio"
+		end
+ 		
+ 		#create issue in github and give it correct tag
+		git_issue = nil
+		case category
+			when 'Bug'
+				git_issue = client.create_issue(repo, title, desc, {:labels => ["bug"]})
+			when 'Enhancement'
+				git_issue = client.create_issue(repo, title, desc, {:labels => ["enhancement"]})
+			when 'Question'
+				git_issue = client.create_issue(repo, title, desc, {:labels => ["question"]})
+			else
+				git_issue = client.create_issue(repo, title, desc)
+		end
+
+		#update the Podio item id with the corresponding github issue id
+		Podio::ItemField.update(params['item_id'], FIELDS_MAP[:issue_number], {:value => git_issue[:number].to_s}, {:hook => false})
+
+		#close the issue on github if the status on Podio is set to complete
+		if status == "Complete"
+			client.close_issue(repo, git_issue[:number])
+		end
+	end
