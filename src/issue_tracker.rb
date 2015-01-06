c68960eb677cf438ce433b3382d3987557b57ea9
homeofmatt <conge101@mail.chapman.edu>
2015-01-05 16:47:59 -0800

    debug

--- src/issue_tracker.rb
+++ src/issue_tracker.rb
@@ -38,7 +38,7 @@ post '/' do
 
 		if curr_rev > 1
 			prev_rev = curr_rev - 1
-			puts issue
+			puts issue.inspect
 			puts ""
 			puts curr_rev.inspect
 			puts Podio::ItemDiff.find_by_item_and_revisions(params['item_id'], prev_rev, curr_rev).inspect
