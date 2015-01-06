f40f7f19cc5cc1cbb3d3acdca0345fde9d6e7cff
homeofmatt <conge101@mail.chapman.edu>
2015-01-06 14:00:23 -0800

    debug

--- src/issue_tracker.rb
+++ src/issue_tracker.rb
@@ -38,7 +38,8 @@ post '/' do
 
 			#if it's an Issue Number or a Project, discard change completely.
 			revision = Podio::ItemDiff.find_by_item_and_revisions(params['item_id'], prev_rev, curr_rev)
-			label = revision[:label].to_s
+			label = revision[:config]["label"]
+			puts label
 
 			chapman_issue = ChapmanPodioIssue.new(params['item_id'], issue, client)
 			chapman_issue.update_on_github(label, revision)
