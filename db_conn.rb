498286cfc3463171860397e86a35664cf010f5e4
homeofmatt <conge101@mail.chapman.edu>
2015-01-09 13:20:03 -0800

    debug

--- db_conn.rb
+++ db_conn.rb
@@ -1,10 +1,10 @@
 db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/id_sets')
 
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-    #:adapter => 'postgresql',
-    #:host => db.host,
-    #:username => db.user,
-    #:database => db.path[1..-1],
-    #:port => db.port,
-    #:encoding => 'utf8'
-#)
+ActiveRecord::Base.establish_connection(
+    :adapter => 'postgresql',
+    :host => db.host,
+    :username => db.user,
+    :database => db.path[1..-1],
+    :port => db.port,
+    :encoding => 'utf8'
+)
