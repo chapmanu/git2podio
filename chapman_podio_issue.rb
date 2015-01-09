f2748f8b564e8fdaffa3b5ecc0cf6a77c82dce26
homeofmatt <conge101@mail.chapman.edu>
2015-01-09 14:19:07 -0800

    debug

--- chapman_podio_issue.rb
+++ chapman_podio_issue.rb
@@ -121,7 +121,8 @@ class ChapmanPodioIssue
 		if label == "Issue Number"
 			# If user tries to change Issue Number, we set it back.
 			prev_num = revision[0][:from][0]["value"]["text"]
-			Podio::ItemField.update(@item_id, FIELDS_MAP[:issue_number], {:value => prev_num}, {:hook => false})
+			puts prev_num
+			#Podio::ItemField.update(@item_id, FIELDS_MAP[:issue_number], {:value => prev_num}, {:hook => false})
 
 		elsif label == "Project"
 			# Same process as Issue Number, but for Repository
