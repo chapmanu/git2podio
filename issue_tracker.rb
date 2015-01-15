98826d6166c7cd848ed8fc7c942f6e08dbfeb9a4
homeofmatt <conge101@mail.chapman.edu>
2015-01-15 14:53:45 -0800

    inside issues

--- issue_tracker.rb
+++ issue_tracker.rb
@@ -90,8 +90,7 @@ post '/github' do
   	#issue = JSON.parse(request.body.read)
 
   	issues_list = client.list_issues("chapmanu/inside")
-  	issues_list.each{ |x| puts x[0]["url"] }
-  	issues_list.each{ |x| puts x[0][:url] }
+  	issues_list.each{ |x| puts x }
 
 
 
