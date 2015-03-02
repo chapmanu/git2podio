d40a01c3d555c29e42ed5d4a1e16c4b3850a9e61
homeofmatt <conge101@mail.chapman.edu>
2015-03-02 13:16:18 -0800

    changed repositories

--- chapman_git_issue.rb
+++ chapman_git_issue.rb
@@ -29,7 +29,7 @@ class ChapmanGitIssue
 		case repo
 			when 'chapmanu/inside'
 				repo_id = 223897464 # Project id for Inside
-			when 'chapmanu/cu-wp-template'
+			when 'chapmanu/cu-wp-customization'
 				repo_id = 223897311 # Project id for Blogs
 			when 'chapmanu/web-components'
 				repo_id = 223898171 # Project id for Homepage
--- chapman_podio_issue.rb
+++ chapman_podio_issue.rb
@@ -65,7 +65,7 @@ class ChapmanPodioIssue
 			when 'Social', 'Inside', 'Events'
 				repo = "chapmanu/inside"
 			when 'Blogs'
-				repo = "chapmanu/cu-wp-template"
+				repo = "chapmanu/cu-wp-customization"
 			when 'Homepage'
 				repo = "chapmanu/web-components"
 			else
